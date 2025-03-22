ecc79e72eff041fece5db3c64db5ec27cd3dc6c2
Axel Velasquez <yoloax11@gmail.com>
2025-03-22 10:01:58 -0700

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/apiRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api', apiRoutes);
 
